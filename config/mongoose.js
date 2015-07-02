c986ac3b008e6cfb4ea66a22efb0cd4fbacb53b6
Alan Yan <yanman333@gmail.com>
2015-07-02 15:34:08 -0400

    seeded book data.

--- config/mongoose.js
+++ config/mongoose.js
@@ -12,10 +12,24 @@ module.exports = function() {
 	// Use Mongoose to connect to MongoDB
 	var db = mongoose.connect(config.db); // connect to MongoDB instance using db property of configuration object
 
-	// // Get an instance of the connection to our database
-	// var db = mongoose.connection;
-
-	// db.on('error', console.error.bind(console, 'Connection error:'));
+	// Create seed data
+	var seedBookData = [
+	  	{
+		    "bookName" : "Harry Potter and the Sorcerer's Stone (#1)",
+		    "authorName" : "J.K. Rowling",
+		    "reviews" : []
+		},
+		{
+			"bookName" : "Divergent (#1)",
+		    "authorName" : "Veronica Roth",
+		    "reviews" : []
+		},
+		{
+			"bookName" : "Twilight (#1)",
+   			"authorName" : "Stephenie Meyer",
+   			"reviews" : []
+   		}
+	];
 	
 	var reviews = require('../models/review');
 	var books = require('../models/book');
@@ -24,67 +38,17 @@ module.exports = function() {
 	var users = require('../models/user');
 
 	// Attach listener to connected event
-	mongoose.connection.once('connected', function() {
-	  console.log("Connected to readig-" + process.env.NODE_ENV + " database...");
+	mongoose.connection.once('connected', function (err) {
+		if (err) throw err;
+
+		books.collection.insertMany(seedBookData, function(err) {
+			if (err) throw err;
+		});
+
+	  	console.log("Connected to readig-" + process.env.NODE_ENV + " database...");
 	});
 
 	// Return the Mongoose connection instance
 	return db;
 }
 
-// var config = require('./config');
-// // mongoose 3.8.x
-// var mongoose = require('mongoose');
-// // mongodb-uri 0.9.x
-// var uriUtil = require('mongodb-uri');
-
-// module.exports = function() {
-// 	// Use Mongoose to connect to MongoDB
-
-// 	/*
-// 	 * Mongoose uses a different connection string format than MongoDB's standard.
-// 	 * Use the mongodb-uri library to help you convert from the standard format to
-// 	 * Mongoose's format.
-// 	 */
-// 	var options = {
-// 		// db: { native_parser: true },
-// 		user: 'myUserName',
-// 		pass: 'myPassword'
-// 	}
-
-// 	var mongodbUri = config.db;
-// 	var mongooseUri = uriUtil.formatMongoose(mongodbUri);
-
-// 	mongoose.connect(mongooseUri, options);
-
-// 	// 	, function (err, res) {
-// 	// 	if (err) { 
-// 	// 	  console.log ('ERROR connecting to: ' + mongooseUri + '. ' + err);
-// 	// 	} else {
-// 	// 	  console.log ('Succeeded connected to: ' + mongooseUri);
-// 	// 	}
-// 	// });
-// 	// Get an instance of the connection to our database
-// 	var db = mongoose.connection; 
-
-// 	db.on('error', console.error.bind(console, 'connection error:')); 
-	
-// 	// load models
-// 	var reviews = require('../models/review');
-// 	var books = require('../models/book');
-// 	var users = require('../models/user');
-
-// 	// Attach listener to connected event
-// 	db.once('open', function() {
-// 		console.log("Connected to readig-" + process.env.NODE_ENV + " database...");
-//   	// Wait for the database connection to establish, then start the app.                         
-// 	});
-
-	
-// 	// mongoose.connection.once('connected', function() {
-// 	//   console.log("Connected to readig-" + process.env.NODE_ENV + " database...");
-// 	// });
-
-// 	// Return the Mongoose connection instance
-// 	return db;
-// }
