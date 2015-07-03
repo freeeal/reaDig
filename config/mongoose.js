39179a23f8e455fc406cd4f581a8288775cd486a
Alan Yan <yanman333@gmail.com>
2015-07-03 19:33:51 -0400

    deleted seed data
    inserting individual books into MongoLab for production / figure out how
    to bulk insert using JSON from CLI

--- config/mongoose.js
+++ config/mongoose.js
@@ -12,60 +12,6 @@ module.exports = function() {
 	// Use Mongoose to connect to MongoDB
 	var db = mongoose.connect(config.db); // connect to MongoDB instance using db property of configuration object
 
-	// Create seed data
-	var seedBookData = [
-	  	{
-		    "bookName" : "Harry Potter and the Sorcerer's Stone (#1)",
-		    "authorName" : "J.K. Rowling",
-		    "reviews" : []
-		},
-		{
-			"bookName" : "Divergent (#1)",
-		    "authorName" : "Veronica Roth",
-		    "reviews" : []
-		},
-		{
-			"bookName" : "Twilight (#1)",
-   			"authorName" : "Stephenie Meyer",
-   			"reviews" : []
-   		},
-   		{
-			"bookName" : "The Giver (#1)",
-    		"authorName" : "Lois Lowry",
-    		"reviews" : []
-   		},
-   		{
-   			"bookName" : "The Fault in Our Stars",
-    		"authorName" : "John Green",
-    		"reviews" : []
-   		},
-   		{
-   			"bookName" : "To Kill a Mockingbird",
-    		"authorName" : "Harper Lee",
-    		"reviews" : []
-    	},
-    	{
-    		"bookName" : "The Book Thief",
-   			"authorName" : "Markus Zusak",
-    		"reviews" : []
-    	},
-    	{
-    		"bookName" : "The Perks of Being a Wallflower",
-    		"authorName" : "Stephen Chbosky",
-    		"reviews" : []
-		},
-		{
-			"bookName" : "The Hunger Games (#1)",
-    		"authorName" : "Suzanne Collins",
-    		"reviews" : []
-		},
-		{
-			"bookName" : "The Hobbit",
-    		"authorName" : "J.R.R. Tolkien",
-    		"reviews" : []
-		}
-	];
-	
 	var reviews = require('../models/review');
 	var books = require('../models/book');
 	
@@ -75,11 +21,6 @@ module.exports = function() {
 	// Attach listener to connected event
 	mongoose.connection.once('connected', function (err) {
 		if (err) throw err;
-
-		books.collection.insertMany(seedBookData, function(err) {
-			if (err) throw err;
-		});
-
 	  	console.log("Connected to readig-" + process.env.NODE_ENV + " database...");
 	});
 
